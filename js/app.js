d5e623422b12c7955bc4b7fc9e3cd0875b095c0f
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2020-09-24 14:16:25 +0200

    new branch

--- js/app.js
+++ js/app.js
@@ -1,96 +1,41 @@
 "use strict";
 
+let serverJSON = '[{"inputs":[{"idInput":"11","hidden":1,"required":1},{"idInput":"12","hidden":0,"required":0},{"idInput":"11","hidden":0,"required":0},{"idInput":"12","hidden":0,"required":0}]},{"inputs":[{"idInput":"21","hidden":1,"required":0},{"idInput":"22","hidden":0,"required":1}]}]';
 
-let formInfo = {};
-let selectStart = document.getElementById("start_select");
-let selectedForm = document.getElementById("new_form");
-
-
-//получаем JSON с сервера
-let xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-
-    if (this.readyState == 4 && this.status == 200) {
-        formInfo = JSON.parse(this.responseText);
-    }
-
-    //Заполняем select возможными вариантами форм
-    for (let i = 1; i <= formInfo.length; i++) {
-        let optionForSelect = document.createElement("option");
-        optionForSelect.innerHTML = formInfo[i - 1].name;
-        let att = document.createAttribute("value");
-        att.value = i;
-        optionForSelect.setAttributeNode(att);
-        selectStart.append(optionForSelect);
-    }
-
-};
+let formInfo = JSON.parse(serverJSON),
+    selectStart = document.getElementById("start_select"),
+    selectedForm = document.getElementById("new_form");
 
 
 
-xmlhttp.open("GET", "data.json", true);
-xmlhttp.send();
-
-
+console.log(formInfo);
 
 
 
 function showForms() {
     let x = document.getElementById("start_select").value;
-    cleanForm();
 
 
     for (let i = 0; i < formInfo[x - 1].inputs.length; i++) {
 
+        let idInput = formInfo[x - 1].inputs[i].idInput,
+            hiddenInput = +formInfo[x - 1].inputs[i].hidden,
+            requiredInput = +formInfo[x - 1].inputs[i].required,
+            inputEdit = document.getElementById(idInput);
 
-        let newInput = document.createElement("input");
-
-
-        newInput.classList.add("form-control");
-
-        let idAtt = document.createAttribute("id");
-        idAtt.value = formInfo[x - 1].inputs[i].type + i;
-        newInput.setAttributeNode(idAtt);
-
-        let typeAtt = document.createAttribute("type");
-        typeAtt.value = formInfo[x - 1].inputs[i].type;
-        newInput.setAttributeNode(typeAtt);
-
-        let typePlaceholder = document.createAttribute("placeholder");
-        typePlaceholder.value = formInfo[x - 1].inputs[i].placeholder;
-        newInput.setAttributeNode(typePlaceholder);
-
-        if (formInfo[x - 1].inputs[i].value !== "") {
-            let valuedAtt = document.createAttribute("value");
-            valuedAtt.value = formInfo[x - 1].inputs[i].value;
-            newInput.setAttributeNode(valuedAtt);
-        }
-
-        if (formInfo[x - 1].inputs[i].required == 1) {
-            let requiredAtt = document.createAttribute("required");
-            newInput.setAttributeNode(requiredAtt);
+        if (requiredInput == 1) {
+            hiddenInput = 0;
+            inputEdit.setAttribute("required", "");
         } else {
-            if (formInfo[x - 1].inputs[i].hidden == 1) {
-                let hiddenAtt = document.createAttribute("type");
-                hiddenAtt.value = "hidden";
-                newInput.setAttributeNode(hiddenAtt);
-            }
+            inputEdit.removeAttribute("required", "");
         }
 
-        if (formInfo[x - 1].inputs[i].hidden == 0) {
-            let newLabelForInput = document.createElement("label");
-
-            let labelForAtt = document.createAttribute("for");
-            labelForAtt.value = formInfo[x - 1].inputs[i].type + i;
-            newLabelForInput.setAttributeNode(labelForAtt);
-
-            newLabelForInput.textContent = formInfo[x - 1].inputs[i].label;
-            selectedForm.append(newLabelForInput);
-            
-
+        if (hiddenInput == 1) {
+            inputEdit.setAttribute("type", "hidden");
+        } else {
+            inputEdit.removeAttribute("type", "hidden");
         }
 
-        selectedForm.append(newInput);
 
     }
 
@@ -98,31 +43,5 @@ function showForms() {
 
 
 
-
-
-
-
-
-    let newSubmit = document.createElement("input");
-    let typeSub = document.createAttribute("type");
-    typeSub.value = "Submit";
-    newSubmit.setAttributeNode(typeSub);
-    selectedForm.append(newSubmit);
-    newSubmit.classList.add("btn");
-    newSubmit.classList.add("btn-primary");
-
-
-    document.getElementById("demo3").innerHTML =  "Вы находитесь в " + formInfo[x - 1].name;
-}
-
-
-function cleanForm() {
-    let inputsInForm = selectedForm.querySelectorAll("input");
-    for (let i = 0; i < inputsInForm.length; i++) {
-        inputsInForm[i].remove();
-    }
-    let labelsInForm = selectedForm.querySelectorAll("label");
-    for (let i = 0; i < labelsInForm.length; i++) {
-        labelsInForm[i].remove();
-    }
+    document.getElementById("demo3").innerHTML = "Вы находитесь в " + x + " выборе";
 }
