f1bc81171c027577de11c0e27d8f2f75f82c2f9a
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2020-09-24 14:24:14 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -1,21 +1,13 @@
 "use strict";
 
-let serverJSON = '[{"inputs":[{"idInput":"11","hidden":1,"required":1},{"idInput":"12","hidden":0,"required":0},{"idInput":"11","hidden":0,"required":0},{"idInput":"12","hidden":0,"required":0}]},{"inputs":[{"idInput":"21","hidden":1,"required":0},{"idInput":"22","hidden":0,"required":1}]}]';
+let serverJSON = '[{"inputs":[{"idInput":"11","hidden":0,"required":1},{"idInput":"12","hidden":0,"required":0},{"idInput":"21","hidden":0,"required":1},{"idInput":"22","hidden":0,"required":0}]},{"inputs":[{"idInput":"11","hidden":1,"required":0},{"idInput":"12","hidden":1,"required":0},{"idInput":"21","hidden":0,"required":1},{"idInput":"22","hidden":0,"required":1}]}]';
 
 let formInfo = JSON.parse(serverJSON),
     selectStart = document.getElementById("start_select"),
     selectedForm = document.getElementById("new_form");
 
-
-
-console.log(formInfo);
-
-
-
 function showForms() {
     let x = document.getElementById("start_select").value;
-
-
     for (let i = 0; i < formInfo[x - 1].inputs.length; i++) {
 
         let idInput = formInfo[x - 1].inputs[i].idInput,
@@ -36,12 +28,6 @@ function showForms() {
             inputEdit.removeAttribute("type", "hidden");
         }
 
-
     }
-
-
-
-
-
-    document.getElementById("demo3").innerHTML = "Вы находитесь в " + x + " выборе";
+    // document.getElementById("demo3").innerHTML = "Вы находитесь в " + x + " выборе";
 }
